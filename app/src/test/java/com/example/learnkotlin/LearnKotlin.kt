3c8458d3281740b01c15cbb374b740237971d4ad
Hans Naert <hans.naert@vives.be>
2026-03-20 09:41:45 +0100

    Add higherorder functions examples (handbook the first line of android code - chapter 6.5)

--- app/src/test/java/com/example/learnkotlin/LearnKotlin.kt
+++ app/src/test/java/com/example/learnkotlin/LearnKotlin.kt
@@ -99,4 +99,60 @@ class LearnKotlin {
         Read(student)
 
     }
+
+    fun plus(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+    fun minus(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+
+    fun example(func: (String, Int) -> Unit) {
+        func("hello", 123)
+    }
+
+    fun num1AndNum2(num1: Int, num2: Int, operation: (Int, Int) -> Int): Int {
+        val result = operation(num1, num2)
+        return result
+    }
+
+    //extension function to StringBuilder
+    //Function Literal with Receiver: "block: StringBuilder.() -> Unit" - this inside lamda points to stringbuilder
+    fun StringBuilder.build(block: StringBuilder.() -> Unit): StringBuilder {
+        block()
+        return this
+    }
+
+    fun StringBuilder.build_apply(block: StringBuilder.() -> Unit): StringBuilder = apply {
+        block()
+    }
+
+    @Test
+    fun higherorderfunctions()
+    {
+        example { a, b -> println("$a $b") }
+
+        val num1 = 100
+        val num2 = 80
+        val result1 = num1AndNum2(num1, num2, ::plus)
+        val result2 = num1AndNum2(num1, num2, ::minus)
+        println("result1 is $result1")
+        println("result2 is $result2")
+        val result3 = num1AndNum2(num1, num2) { n1, n2 -> n1 + n2 }
+        val result4 = num1AndNum2(num1, num2) { n1, n2 -> n1 - n2 }
+        println("result1 is $result3")
+        println("result2 is $result4")
+
+        val list = listOf("Apple", "Banana", "Orange", "Pear", "Grape")
+        val result = StringBuilder().build_apply {
+            append("Start eating fruits.\n")
+            for (fruit in list) {
+                append(fruit).append("\n")
+            }
+            append("Ate all fruits.")
+        }
+        println(result.toString())
+
+    }
+
 }
